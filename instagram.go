db12926f2a88e3a3f172a1e4ab4e65025a1a6bf6
luifa <luifa.luifa@gmail.com>
2020-12-06 22:13:11 +0100

    add validation for empty results

--- instagram.go
+++ instagram.go
@@ -15,34 +15,38 @@ func getPostsFromInstagram(urlToCrawl string) []Post {
 
 	c.OnResponse(func(r *colly.Response) {
 		htmlString := string(r.Body)
-		jsonData := htmlString[strings.Index(htmlString, "window._sharedData =")+len("window._sharedData =") : len(htmlString)]
-		jsonData = strings.TrimSpace(jsonData[0:strings.Index(jsonData, ";</script>")])
+		if (strings.Index(htmlString, "window._sharedData =")) > 0 {
+			jsonData := htmlString[strings.Index(htmlString, "window._sharedData =")+len("window._sharedData =") : len(htmlString)]
+			jsonData = strings.TrimSpace(jsonData[0:strings.Index(jsonData, ";</script>")])
 
-		jsonData = jsonData[strings.Index(jsonData, "\"edge_hashtag_to_media\":")+len("\"edge_hashtag_to_media\":") : len(jsonData)]
-		jsonData = strings.TrimSpace(jsonData[0:strings.Index(jsonData, ",\"edge_hashtag_to_top_posts\"")])
-		byt := []byte(jsonData)
-		var payload map[string]interface{}
-		if err := json.Unmarshal(byt, &payload); err != nil {
-			panic(err)
-		}
-		postsList := payload["edges"].([]interface{})
+			if strings.Index(jsonData, "\"edge_hashtag_to_media\":") > 0 {
+				jsonData = jsonData[strings.Index(jsonData, "\"edge_hashtag_to_media\":")+len("\"edge_hashtag_to_media\":") : len(jsonData)]
+				jsonData = strings.TrimSpace(jsonData[0:strings.Index(jsonData, ",\"edge_hashtag_to_top_posts\"")])
+				byt := []byte(jsonData)
+				var payload map[string]interface{}
+				if err := json.Unmarshal(byt, &payload); err != nil {
+					panic(err)
+				}
+				postsList := payload["edges"].([]interface{})
 
-		postsToSave := MaxPostsSize
-		if len(postsList) < MaxPostsSize {
-			postsToSave = len(postsList)
-		}
-		for i := 0; i < postsToSave; i++ {
-			postJSON := postsList[i].(map[string]interface{})
-			node := postJSON["node"].(map[string]interface{})
-			takenAt := int64(node["taken_at_timestamp"].(float64))
-			datum := time.Unix(takenAt, 0).Format("02.01.2006 15:04:05")
-			link := "https://www.instagram.com/p/" + node["shortcode"].(string)
-			title := link
-			if len(title) > MaxTitleLenght {
-				title = title[0:MaxTitleLenght] + "..."
+				postsToSave := MaxPostsSize
+				if len(postsList) < MaxPostsSize {
+					postsToSave = len(postsList)
+				}
+				for i := 0; i < postsToSave; i++ {
+					postJSON := postsList[i].(map[string]interface{})
+					node := postJSON["node"].(map[string]interface{})
+					takenAt := int64(node["taken_at_timestamp"].(float64))
+					datum := time.Unix(takenAt, 0).Format("02.01.2006 15:04:05")
+					link := "https://www.instagram.com/p/" + node["shortcode"].(string)
+					title := link
+					if len(title) > MaxTitleLenght {
+						title = title[0:MaxTitleLenght] + "..."
+					}
+					postToReturn := Post{Date: datum, Title: title, Link: link}
+					postsToReturn = append(postsToReturn, postToReturn)
+				}
 			}
-			postToReturn := Post{Date: datum, Title: title, Link: link}
-			postsToReturn = append(postsToReturn, postToReturn)
 		}
 	})
 
